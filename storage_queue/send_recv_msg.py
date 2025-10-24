2410799d32b1c0d11aa12e1bfba879f30f773a6c
goformunish <41254403+goformunish@users.noreply.github.com>
2025-10-24 22:43:58 +0200

    Update send_recv_msg.py

--- storage_queue/send_recv_msg.py
+++ storage_queue/send_recv_msg.py
@@ -3,7 +3,7 @@ from azure.storage.blob import BlobServiceClient
 from azure.storage.queue import QueueServiceClient
 
 # %%
-conn_str_queue='BlobEndpoint=https://mystore553.blob.core.windows.net/;QueueEndpoint=https://mystore553.queue.core.windows.net/;FileEndpoint=https://mystore553.file.core.windows.net/;TableEndpoint=https://mystore553.table.core.windows.net/;SharedAccessSignature=sv=2024-11-04&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2025-10-25T03:54:05Z&st=2025-10-24T19:39:05Z&spr=https&sig=pYTbE2gcASimB%2Fnx5HG6lQ9XdMhXRseLle01i%2FBWtf8%3D'
+conn_str_queue=''
 
 # %%
 blob_service_client=BlobServiceClient.from_connection_string(conn_str_queue)
